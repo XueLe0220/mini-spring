1329f44f789e6adbef6c59251ee832eaf6d0613f
xuele <2125247514@qq.com>
2025-12-08 20:59:28 +0800

    feat:分包优化

--- test-demo/src/main/java/cn/xuele/test/common/MyBeanPostProcessor.java
+++ test-demo/src/main/java/cn/xuele/test/common/MyBeanPostProcessor.java
@@ -1,4 +1,6 @@
-import core.processor.BeanPostProcessor;
+package cn.xuele.test.common;
+
+import cn.xuele.minispring.beans.factory.config.BeanPostProcessor;
 
 /**
  * @author XueLe
@@ -8,7 +10,7 @@ public class MyBeanPostProcessor implements BeanPostProcessor {
     @Override
     public Object postProcessBeforeInitialization(Object bean, String beanName) {
         if ("userService".equals(beanName)) {
-            System.out.println(">> [BPP] 在初始化之前，我把 UserService 拦截下来了！");
+            System.out.println(">> [BPP] 在初始化之前，我把 cn.xuele.test.bean.UserService 拦截下来了！");
         }
         return bean;
     }
